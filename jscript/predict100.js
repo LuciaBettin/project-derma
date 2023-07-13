fb12811c75922092f4a74fd6652605391a00d5e4
Lucia Ramos Betin <daniela.ramos@mercadolibre.com.co>
2023-07-13 14:34:39 -0500

    Update and rename predict1000.js to predict100.js

--- jscript/predict100.js
+++ jscript/predict100.js
@@ -40,7 +40,7 @@ $("#image-selector").change(function () {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('model_kerasnative_v4/model.json');
+	model = await tf.loadModel('model_kerasnative_v4_2/model.json');
 	$("#selected-image").attr("src", "assets/samplepic.jpg")
 	
 	
